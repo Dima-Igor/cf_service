777063825af9ae1b66e6e895d0caebd3b1080505
Skeef79 <theskeef79@gmail.com>
2022-01-11 03:16:01 +0500

    cf service

---
+++ cf_api.py
@@ -0,0 +1,79 @@
+from asyncio.windows_events import NULL
+from re import sub
+import aiohttp
+import asyncio
+import requests
+from collections import namedtuple
+import time
+
+class CodeforcesAPI:
+    def __init__(self):
+        pass
+    
+    def api_response(self, url, params=None):
+        try:
+            tries = 0
+            while tries < 5:
+                tries += 1
+                resp = requests.get(url)
+                response = {}
+                if resp.status_code == 503:
+                    response['status'] = "FAILED"
+                    response['comment'] = "limit exceeded"
+                else:
+                    response = resp.json()
+                
+                if response['status'] == 'FAILED' and 'limit exceeded' in response['comment'].lower():
+                    time.sleep(1)
+                else:
+                    return response
+            return response
+        except Exception as e:
+            return None
+
+    def get_user_submissions(self, handle):
+        url = f"https://codeforces.com/api/user.status?handle={handle}"
+        response = self.api_response(url)
+        if not response:
+            return [False, "CF API Error"]
+        if response['status'] != 'OK':
+            return [False, response['comment']]
+        try:
+            data = []
+            for x in response['result']:
+                y = x['problem']
+                problem = {}
+                
+                problem["handle"] = handle
+                
+                if "problem_rating" in y:
+                    problem["problem_rating"] = y["rating"]
+                else:
+                    problem["problem_rating"] = None
+                
+                if "contestId" in y:
+                    problem["contest_id"] = y["contestId"]
+                else:
+                    problem["contest_id"] = None
+
+                if "index" in y:
+                    problem["problem_index"] = y["index"]
+                else:
+                    problem["problem_index"] = None
+
+                if "sub_time" in x:
+                    problem["sub_time"] = x["creationTimeSeconds"]
+                else:
+                    problem["sub_time"] = None
+
+                if 'verdict' in x:
+                    problem['verdict'] = x['verdict']
+                else:
+                    problem['verdict'] = None
+
+                data.append(problem)
+            return [True, data]
+        except Exception as e:
+            print('hereis exception')
+            return [False, str(e)]
+
---
+++ cf_service.py
@@ -0,0 +1,63 @@
+from cf_api import CodeforcesAPI
+import asyncio
+import websockets
+import time
+import datetime
+import json
+import grpc
+import submission_pb2
+import submission_pb2_grpc
+from concurrent import futures
+
+
+class CodeforcesService(submission_pb2_grpc.CodeforcesService):
+    def __init__(self):
+        self.cfApi = CodeforcesAPI()
+
+    def GetSubmissions(self, request, context):
+        status, submissions = self.cfApi.get_user_submissions(request.handle)
+        
+        if not status:
+            print(submissions)
+            if submissions == "CF API Error":
+                for i in range(1):
+                    yield submission_pb2.SubmissionReply(
+                        status="CF API Error"
+                    )
+            elif submissions == f"handle: User with handle {request.handle} not found":
+                for i in range(1):
+                    yield submission_pb2.SubmissionReply(
+                        status="User not found"
+                    )
+            else:
+                for i in range(1):
+                    yield submission_pb2.SubmissionReply(
+                        status="Unknown error"
+                    )
+        else:
+            print("Sending submissions")
+            for submission in submissions:
+                yield submission_pb2.SubmissionReply(
+                    handle=submission["handle"],
+                    contest_id=submission["contest_id"],
+                    problem_index=submission["problem_index"],
+                    sub_time=submission["sub_time"],
+                    verdict=submission["verdict"],
+                    status="OK"
+                )
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    submission_pb2_grpc.add_CodeforcesServiceServicer_to_server(
+        CodeforcesService(), server)
+
+    addr = "localhost:8090"
+    print(f"Codeforces service is listening on {addr}")
+    server.add_insecure_port(addr)
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
